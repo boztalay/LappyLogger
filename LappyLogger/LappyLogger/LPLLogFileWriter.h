27637938945eed714c805ab1e96d820b76f43dd3
Ben Oztalay <boztalay@gmail.com>
2014-10-03 02:54:37 -0400

    Made a log file writer, got rid of the original log file manager

---
+++ LappyLogger/LappyLogger/LPLLogFileWriter.h
@@ -0,0 +1,13 @@
+//
+//  LPLLogFileWriter.h
+//  LappyLogger
+//
+//  Created by Ben Oztalay on 10/3/14.
+//  Copyright (c) 2014 Ben Oztalay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LPLLogFileWriter : NSObject
+
+@end
---
+++ LappyLogger/LappyLogger/LPLLogFileWriter.m
@@ -0,0 +1,13 @@
+//
+//  LPLLogFileWriter.m
+//  LappyLogger
+//
+//  Created by Ben Oztalay on 10/3/14.
+//  Copyright (c) 2014 Ben Oztalay. All rights reserved.
+//
+
+#import "LPLLogFileWriter.h"
+
+@implementation LPLLogFileWriter
+
+@end
