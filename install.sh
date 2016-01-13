4b395c1e13f8a03a89630dd8719f2bc0234aa90d
Ben Oztalay <boztalay@gmail.com>
2016-01-13 15:27:14 -0500

    Adding a symbolic link to the new binary location

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 cp -r ./LappyLoggerRelease/Products/Applications/LappyLogger.app /Applications/LappyLogger.app
 cp ./LappyLoggerRelease/Products/Library/LaunchDaemons/com.boztalay.LappyLogger.plist /Library/LaunchDaemons/
+ln -s /Applications/LappyLogger.app/LappyLogger /usr/local/bin/LappyLogger
